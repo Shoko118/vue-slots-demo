7629bbdd454c44c8d3b595ee52a327affde4ccb2
Shoko118 <secast118@gmail.com>
2021-12-24 12:11:21 +0800

    self tutorial for vue slots

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Welcome>
+    <template #demo>
+      <h1>template name</h1>
+    </template>
+    <template #firstName="{ middleName, lastName }">
+      <h1>Slot firstName of middel name {{ middleName }}</h1>
+      <h1>Slot firstName of last name {{ lastName }}</h1>
+    </template>
+  </Welcome>
+</template>
+
+<script setup>
+import Welcome from "./components/Welcome.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hello welcome components</h1>
+    <!-- V-slots -->
+    <slot name="demo"></slot>
+    <slot name="firstName" :middleName="leon" :lastName="woo"> </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const leon = "leon";
+    const woo = "woo";
+    return {
+      leon,
+      woo,
+    };
+  },
+};
+</script>
